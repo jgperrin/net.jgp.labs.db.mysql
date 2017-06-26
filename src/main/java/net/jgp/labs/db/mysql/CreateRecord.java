1893d3c195c740dda05383cf44135bc989becc55
Jean Georges Perrin <jgp@jgp.net>
2017-06-26 07:34:16 -0400

    Still work in progress, not good to go.

--- src/main/java/net/jgp/labs/db/mysql/CreateRecord.java
+++ src/main/java/net/jgp/labs/db/mysql/CreateRecord.java
@@ -5,8 +5,8 @@ import java.sql.PreparedStatement;
 public class CreateRecord {
 	public boolean dboperation(String query) {
 		
-		PreparedStatement preparedStatement = ConnectionManager.getConnection()
-                .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
+//		PreparedStatement preparedStatement = ConnectionManager.getConnection()
+//                .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
 		
 		return true;
 	}
